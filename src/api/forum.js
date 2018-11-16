0819043f74827f301995474bf11a61c8485109a1
Alex Roman <eirexe123@gmail.com>
2018-11-16 01:04:32 +0100

    No usar fuzzy para hilos

--- src/api/forum.js
+++ src/api/forum.js
@@ -79,7 +79,7 @@ export default {
     return API.makePOST(`/api/users/${user.username}/ban_user/`, payload)
   },
   async threadSearch (text = '', username = '', forum = '', page = 1) {
-    return API.makeGET(`/api/search/threads/?text__fuzzy=${text}&username=${username}&forum=${forum}&page=${page}`)
+    return API.makeGET(`/api/search/threads/?text=${text}&username=${username}&forum=${forum}&page=${page}`)
   },
   async userSearch (text = '', page = 1) {
     return API.makeGET(`/api/search/users/?text__fuzzy=${text}&page=${page}`)
