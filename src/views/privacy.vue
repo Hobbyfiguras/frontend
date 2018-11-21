b0982f020a7bd7d4dcf38986bb297d63f61bc333
Alex Roman <eirexe123@gmail.com>
2018-11-21 01:12:32 +0100

    Politica de privacidad actualizada

--- src/views/privacy.vue
+++ src/views/privacy.vue
@@ -4,101 +4,97 @@
     <div class="card">
       <div class="card-content">
         <div class="content">
-          <h1 class="">Privacy Policy</h1>
-          <p>Effective Date: August 12, 2018</p>
-          <h2 class="">This Privacy Policy describes</h2>
+          <h1 class="">Politica de privacidad</h1>
+          <p>Fecha efectiva: 21 de Noviembre de 2018</p>
+          <h2 class="">Esta politica de privacidad describe</h2>
           <ul>
-            <li>The ways we collect personal data about you and why we collect it</li>
-            <li>How your personal data is used</li>
-            <li>The choices you have about your personal data</li>
+            <li>Como recopilamos sus datos y para que</li>
+            <li>Como se utilizan sus datos personales</li>
+            <li>Las elecciones que tiene sobre sus datos personales</li>
           </ul>
-          <p>This Privacy Policy applies to EIRTeam's games, websites and related services, which we here collectively call the Service. We may update this Privacy Notice from time to time in response to changing legal, technical or business developments. When we update our Privacy Notice, we will take appropriate measures to inform you, consistent with the significance of the changes we make. We will obtain your consent to any material Privacy Notice changes if and where this is required by applicable data protection laws.</p>
-          <p>If you have any questions or concerns about our use of your personal information, please contact us using the contact details provided at the bottom of this Privacy Notice.</p>
-          <h2 class="">THE DATA WE COLLECT</h2>
-          <h3 class="">That you provide </h3>
+          <p>Esta Politica de Privacidad aplica a los sitios web y otros servicios relacionados de Hobbyfiguras, a los cuales nos referimos colectivamente como el Servicio. Podemos actualizar esta Politica de Privacidad de vez en cuando en respuesta a cambios en desarrollos legales, técnicos o comerciales. Cuando actualizamos nuestro Aviso de Privacidad, tomaremos las medidas adecuadas para informarle, de acuerdo con la importancia de los cambios que realicemos. Recibiremos su consentimiento para cualquier material de aviso de privacidad que usted modifique, siempre y cuando lo exija la legislación de protección de datos aplicable.</p>
+          <p>Si tiene alguna pregunta o inquietud sobre el uso que hacemos de su información personal, comuníquese con nosotros utilizando los datos de contacto que se encuentran al final de esta Politica de Privacidad.</p>
+          <h2 class="">THE DATA WE RECOPILAMOS</h2>
+          <h3 class="">Que proporciona usted </h3>
           <ul>
-            <li>Profile information such as profile photos, usernames or social media links</li>
-            <li>New account names or passwords</li>
-            <li>Contact information such as your name or email address</li>
-            <li>For store purchases, real name, mailing address and/or telephone number</li>
-            <li>Additional data that you provide through our service like forum posts, email messages, uploaded images or website links.</li>
-            <li>Other data that you choose to give us (such as data to identify a lost account)</li>
+            <li> Información de perfil, como fotos de perfil, nombres de usuario o enlaces de redes sociales </li>
+             <li> Nuevos nombres de cuenta o contraseñas </li>
+             <li> Información de contacto, como su nombre o dirección de correo electrónico </li>
+             <li> Los datos adicionales que proporciona a través de nuestro servicio, como publicaciones en foros, mensajes de correo electrónico, imágenes cargadas o enlaces a sitios web. </li>
+             <li> Otros datos que elige proporcionarnos (como los datos para identificar una cuenta perdida) </li>
           </ul>
-          <h3>That we collect automatically</h3>
+          <h3>Que recopilamos automaticamente</h3>
           <ul>
-            <li>Data about your account</li>
-            <li>IP address (General geographic location)</li>
-            <li>System specifications (Including mobile device identifiers)</li>
-            <li>Browser information</li>
-            <li>Gameplay data and your interactions with other players inside the Service</li>
+            <li> Datos sobre su cuenta </li>
+            <li> dirección IP (ubicación geográfica general) </li>
+            <li> Especificaciones del sistema (incluidos los identificadores de dispositivos móviles) </li>
+            <li> Información del navegador </li>
+            <li> Datos de uso y sus interacciones con otros usuarios dentro del Servicio </li>
           </ul>
-          <p>In most cases, we also create an EIRTeam ID for you when you use the Service.</p>
-          <h3>Data we collect from our partners</h3>
+          <p>En la mayoría de los casos, también creamos un ID de Hobbyfiguras cuando usa el Servicio.</p>
+          <h3> Datos que recopilamos de nuestros socios </h3>
           <ul>
-            <li>Data we receive if you link a third party tool with the Service (such as facebook, Steam or Google)</li>
-            <li>Social media analytics data</li>
-            <li>Data for the purpose of security such as spam blacklists</li>
-            <li>Data from game platform services such as Steam profile information, game ownership, item inventory</li>
-            <li>Data from platforms that the games run on (such as to verify payment)</li>
+            <li> Los datos que recibimos si vincula una herramienta de terceros con el Servicio (como Facebook, Steam o Google) </li>
+            <li> Datos de análisis de redes sociales </li>
+            <li> Datos para fines de seguridad, como listas negras de spam </li>
           </ul>
-          <h2>WHY WE COLLECT THIS DATA</h2>
-          <h3>We collect this data in order to operate game services</h3>
+          <h2> POR QUÉ RECOPILAMOS ESTOS DATOS </h2>
+          <h3> Recopilamos estos datos para hacer funcionar el servicio </h3>
           <ul>
-            <li>Create accounts and allow players to use our service</li>
-            <li>Confirm ownership and account entitlements</li>
-            <li>Provide requested services</li>
-            <li>Inform players of service related information</li>
+            <li> Crea cuentas y permitir que los usuarios usen nuestro servicio </li>
+            <li> Confirmar propiedad y derechos de cuenta </li>
+            <li> Proporcionar servicios solicitados </li>
+            <li> informar a los usuarios de la información relacionada con el servicio </li>
           </ul>
-          <h3>Provide ongoing game support for all players</h3>
+          <h3> Proporcionar soporte continuo para todos los usuarios </h3>
+           <ul>
+             <li> Proporcionar a los usuarios métodos para interactuar entre sí </li>
+             <li> Gestiona nuestra relación contigo </li>
+             <li> Comprender cómo nuestros usuarios en conjunto interactúan con nuestros servicios </li>
+             <li> Proporcionar herramientas y métodos para comunicarnos con nuestros usuarios </li>
+             <li> Proporcione alertas de seguridad, actualizaciones y noticias </li>
+           </ul>
+          <h3> Seguridad y protección </h3>
           <ul>
-            <li>Provide players methods to interact with each other</li>
-            <li>Manage our relationship with you</li>
-            <li>Understand how our players as a whole interact with our services</li>
-            <li>Provide tools and methods in which to communicate with our players</li>
-            <li>Provide security alerts, product updates and news</li>
+            <li> Prevenir usuarios maliciosos </li>
+            <li> Moderar las comunicaciones entre los usuarios </li>
+            <li> Fomentar un entorno seguro y amigable entre los usuarios </li>
           </ul>
-          <h3>Security and safety</h3>
+          <h3> Comportamiento del usuarios y datos históricos </h3>
           <ul>
-            <li>Prevent malicious users</li>
-            <li>Moderate player discussions</li>
-            <li>Foster a safe and friendly environment between users</li>
+            <li> Tomar decisiones inteligentes e informadas sobre nuestras operaciones de servicio </li>
+            <li> Responde a los comentarios y sugerencias de los usuarios </li>
           </ul>
-          <h3>Player behavior and historical data</h3>
-          <ul>
-            <li>Make smart informed choices about our service operations</li>
-            <li>Respond to player feedback and suggestions</li>
-          </ul>
-          <h2>WHO CAN SEE YOUR DATA</h2>
-          <p>Apart from EIRTeam, your data can be accessed by others in the following situations:</p>
-          <h3>Other players and users</h3>
-          <p>If our game includes social features, other players and users may see limited data. Other players and users may, for example, see your profile data, in-game activities and read messages you have posted.</p>
-          <h3>Partners working with EIRTeam</h3>
-          <p>EIRTeam has partners to perform services for us. These partners process your data only at and according to our instructions to provide the Service, such as hosting, player support, analytics and fraud prevention.</p>
-          <h3>Other companies and public authorities</h3>
-          <p>In order to combat fraud and illegal activity, we may exchange data with other companies and organizations and provide it to public authorities in response to lawful requests.</p>
-          <p>We may also disclose your data based on your consent, to comply with the law or to protect the rights, property or safety of us, our players or others.</p>
-          <h3>Anonymized/Aggregated Data</h3>
-          <p>We may share aggregate or de-identified information with third parties for research, marketing, analytics and other purposes, provided such information does not identify a particular individual.</p>
-          <h3>International Data Transfers</h3>
-          <p>Our Service is global by nature and your data can therefore be transferred to anywhere in the world. Because different countries may have different data protection laws than your own country, we take steps to ensure adequate safeguards are in place to protect your data as explained in this Policy. Adequate safeguards that our partners may use include standard contractual clauses approved by EU Commission and the Privacy Shield certification in case of transfers to the USA.</p>
-          <h2>YOUR RIGHTS AND OPTIONS</h2>
-          <h3>Opt-out of marketing emails and other direct marketing</h3>
-          <p>You may opt-out of receiving promotional communications, such as marketing emails from us by following the instructions in such communications. You can always withdraw your consent, for example by clicking the unsubscribe link on an email or opting out of sending data in-game.</p>
-          <h3>Access the personal data we hold about you</h3>
-          <p>If you request, we will provide you a copy of your personal data in an electronic format.</p>
-          <h3>Your other rights</h3>
-          <p>You also have the right to correct your data, have your data deleted, object how we use or share your data, and restrict how we use or share your data.</p>
-          <p>Requests may be submitted to legal@ero-one.moe - We will respond to all requests, inquiries or concerns within thirty (30) days.</p>
-          <h2>COOKIES AND SIMILAR TECHNOLOGY</h2>
-          <p>Like most online services, we and our partners use cookies and similar technologies to provide and personalize the Service, analyse use, target advertisements and prevent fraud. You can disable cookies in your browser settings, but some parts of the Service may then not function properly.</p>
-          <h2>HOW DO WE PROTECT YOUR DATA</h2>
-          <h3>Security Safeguards</h3>
-          <p>In order to help ensure a secure and safe player experience, we are continuously developing and implementing administrative, technical and physical security measures to protect your data from unauthorized access or against loss, misuse or alteration.</p>
-          <h2>AGE LIMITS</h2>
-          <p>We do not knowingly collect or solicit personal data about or direct or target interest based advertising to anyone under the age of 18 or knowingly allow such persons to use our Services. If you are under 18, please do not send any data about yourself to us, including your name, address, telephone number, or email address. No one under the age of 18 may provide any personal data. If we learn that we have collected personal data about a minor under age 18, we will delete that data as quickly as possible. If you believe that we might have any data from or about a minor under the age of 18, please contact us.</p>
-          <h2>CONTACT US</h2>
-          <p>If you have any questions or concerns about our use of your personal information or regarding the privacy policy, please contact us at <a href="mailto:admin@ero-one.moe">admin@ero-one.moe</a></p>
-        </div>
+          <h2> QUIÉN PUEDE VER SUS DATOS </h2>
+          <p> Además de Hobbyfiguras, otras personas pueden acceder a sus datos en las siguientes situaciones: </p>
+          <h3> Otros usuarios </h3>
+          <p> Otros usuarios pueden, por ejemplo, ver los datos de tu perfil, las actividades del Servicio y leer los mensajes que hayas publicado. </p>
+          <h3> Socios que trabajan con Hobbyfiguras </h3>
+          <p> Hobbyfiguras tiene socios que realizar servicios para nosotros. Estos socios procesan sus datos solo en y de acuerdo con nuestras instrucciones para proporcionar el Servicio, como alojamiento, asistencia al usuario, análisis y prevención de fraude. </p>
+          <h3> Otras empresas y autoridades públicas </h3>
+          <p> Para combatir el fraude y la actividad ilegal, podemos intercambiar datos con otras compañías y organizaciones y proporcionarlos a las autoridades públicas en respuesta a solicitudes legales. </p>
+          <p> También podemos divulgar sus datos en función de su consentimiento, para cumplir con la ley o para proteger los derechos, la propiedad o la seguridad de nosotros, nuestros usuarios u otros. </p>
+          <h3> Datos anónimos / agregados </h3>
+          <p> Podemos compartir información agregada o no identificable con terceros para fines de investigación, marketing, análisis y otros, siempre que dicha información no identifique a una persona en particular. </p>
+          <h3> Transferencias internacionales de datos </h3>
+          <p> Nuestro Servicio es global por naturaleza y, por lo tanto, sus datos pueden transferirse a cualquier parte del mundo. Debido a que diferentes países pueden tener leyes de protección de datos diferentes a las de su propio país, tomamos medidas para garantizar que existan salvaguardas adecuadas para proteger sus datos como se explica en esta Política. Las garantías adecuadas que nuestros socios pueden usar incluyen cláusulas contractuales estándar aprobadas por la Comisión de la UE y la certificación Privacy Shield en caso de transferencias a los EE. UU. </p>
+          <h2> SUS DERECHOS Y OPCIONES </h2>
+          <h3> Exclusión de correos electrónicos de marketing y otro marketing directo </h3>
+          <p> Usted puede optar por no recibir comunicaciones promocionales, como correos electrónicos de marketing de nuestra parte, siguiendo las instrucciones de dichas comunicaciones. Siempre puede retirar su consentimiento, por ejemplo, haciendo clic en el enlace de cancelación de suscripción en un correo electrónico o inhabilitando el envío de datos dentro del Servicio. </p>
+          <h3> Para acceder a los datos personales que tenemos sobre usted </h3>
+          <p> Si lo solicita, le proporcionaremos una copia de sus datos personales en formato electrónico. </p>
+          <h3> Tus otros derechos </h3>
+          <p> También tiene derecho a corregir sus datos, a que se eliminen sus datos, a objetar cómo usamos o compartimos sus datos, y restringir cómo usamos o compartimos sus datos. </p>
+          <p> Las solicitudes se pueden enviar a legal@hobbyfiguras.moe: responderemos a todas las solicitudes, consultas o inquietudes en menos de (30) días. </p>
+          <h2> COOKIES Y TECNOLOGÍA SIMILAR </h2>
+          <p> Como la mayoría de los servicios en línea, nosotros y nuestros socios utilizamos cookies y tecnologías similares para proporcionar y personalizar el Servicio, analizar el uso, dirigir anuncios y prevenir el fraude. Puede desactivar las cookies en la configuración de su navegador, pero es posible que algunas partes del Servicio no funcionen correctamente. </p>
+          <h2> CÓMO PROTEGEMOS SUS DATOS </h2>
+          <h3> Garantías de seguridad </h3>
+          <p> Para ayudar a garantizar una experiencia de usuario segura, estamos desarrollando e implementando continuamente medidas de seguridad administrativas, técnicas y físicas para proteger sus datos contra el acceso no autorizado o contra la pérdida, mal uso o alteración. </p>
+          <h2> LÍMITES DE EDAD </h2>
+          <p> No recopilamos ni solicitamos a sabiendas información personal ni publicamos publicidad dirigida a personas menores de 14 años, ni permitimos a sabiendas que utilicen nuestros Servicios. Si es menor de 14 años, no nos envíe ningún dato sobre usted, incluido su nombre, dirección, número de teléfono o dirección de correo electrónico. Ninguna persona menor de 14 años puede proporcionar datos personales. Si nos enteramos de que hemos recopilado datos personales sobre un menor de 14 años, eliminaremos esos datos lo más rápido posible. Si cree que podríamos tener datos de o sobre un menor de edad menor de 14 años, contáctenos. </p>
+          <h2> CONTÁCTENOS </h2>
+          <p> Si tiene alguna pregunta o inquietud sobre el uso que hacemos de su información personal o sobre la política de privacidad, contáctenos en <a href="mailto:legal@hobbyfiguras.moe"> legal@hobbyfiguras.moe </a> </p>        </div>
       </div>
     </div>
   </div>
